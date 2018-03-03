a0b6fe63dd0494e8b187872e6a16a8b18653de76
merciof <mercio.andrade@ufrpe.br>
2018-03-03 17:40:24 -0300

    acrescimo do segundo loop, para verificar a possibilidade da construção da nota com as palavras armazenadas no magazineArr.

--- Algoritmos JavaScript/harmlessRansomNote.js
+++ Algoritmos JavaScript/harmlessRansomNote.js
@@ -6,20 +6,34 @@ function harmlessRansomNote(noteText, magazineText) {
     let magazineArr = magazineText.split(' ');
     let magazineObj = {}; //hash table
 
-    magazineArr.forEach(function(word){
+    magazineArr.forEach(function(palavra){
         //caso a palavra nao esteja contida no objeto
-        if (!magazineObj[word]) {
+        if (!magazineObj[palavra]) {
             //a propriedade referente a quantidade de ocorrencias
             //da palavra no texto recebe 0
-            magazineObj[word] = 0;            
+            magazineObj[palavra] = 0;            
         }
         //a propriedade referente a quantidade de ocorrencias
         //da palavra no texto recebe + 1
-        magazineObj[word] = magazineObj[word] + 1;
+        magazineObj[palavra] = magazineObj[palavra] + 1;
     });
 
-    return magazineObj;
+    let noteIsPossible = true;
+    noteArr.forEach(function(palavra){
+        if(magazineObj[palavra]) {
+            magazineObj[palavra]--;
+            if(magazineObj[palavra] < 0) {
+                noteIsPossible = false;
+            }
+        } else {
+            noteIsPossible = false;
+        }
+        
+    });
 };
 
-console.log(harmlessRansomNote('','ola, este eh um texto sobre o vazio sobre este este texto'));
+let noteText = 'this is a secret note for you from a secret admirer';
+let magazineText = 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited';
+
+console.log(harmlessRansomNote(noteText, magazineText));
 
