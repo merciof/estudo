909421b55634acd8f0d2fd2f5a5113afd57387db
merciof <mercio.andrade@ufrpe.br>
2018-03-31 02:07:40 -0300

    definição do elemento que aparece mais vezes no array

--- Algoritmos JavaScript/meanMedianMode.js
+++ Algoritmos JavaScript/meanMedianMode.js
@@ -1,3 +1,11 @@
+function meanMedianMode(array) {
+    return {
+      mean: getMean(array),
+      median: getMedian(array),
+      mode: getMode(array)
+    }
+  }
+
 function getMean(array) {
     var sum = 0;
     array.forEach(function(num){
@@ -27,7 +35,9 @@ function getMedian(array) {
 
 function getMode(array) {
     var modeObj = {};
-
+    var maxFrequency = 0;
+    var modes = [];
+    
     //create modeObj
     array.forEach(function(num) {
         if(!modeObj[num]) {
@@ -37,10 +47,26 @@ function getMode(array) {
         modeObj[num] = modeObj[num] + 1;
     });
 
-    return modeObj;
+    
+    //create array of mode
+    for(var num in modeObj) {
+        if(modeObj[num] > maxFrequency) {
+            modes = [num];
+            maxFrequency = modeObj[num];
+        } else if(modeObj[num] === maxFrequency) {
+            modes.push(num);
+        }
+    }
+
+    if(modes.length === Object.keys(modeObj).length) {
+        modes = [];
+    }
+
+    return modes;
 } 
+
+
 
 var arr = [1,2,3,4,5,1,2,2,2,3,4,10,11,1];
 
 console.log(getMode(arr));
-
