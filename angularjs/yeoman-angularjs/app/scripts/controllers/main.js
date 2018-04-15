515d74aa882981f7b27489cd472beccd5d0af993
merciof <mercio.andrade@ufrpe.br>
2018-04-14 21:41:22 -0300

    instanciação do objeto firebase

--- angularjs/yeoman-angularjs/app/scripts/controllers/main.js
+++ angularjs/yeoman-angularjs/app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the firebaseAppApp
  */
 angular.module('firebaseAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+      //**Forma antiga de instanciar o objeto firebase
+      // var rootRef = new Firebase('https://projeto-teste-7dcf3.firebaseio.com/');
+      // var childRef = rootRef.child('message'); */
+      
+      var config = {
+        apiKey: "AIzaSyD_RJeXxSxpw7LXZ5RWK_zUWwGXR7nv3M4",
+        authDomain: "projeto-teste-7dcf3.firebaseapp.com",
+        databaseURL: "https://projeto-teste-7dcf3.firebaseio.com"
+      };
+      
+      firebase.initializeApp(config);
+
+      var rootRef = firebase.database().ref();
+      var childRef = rootRef.child('message');
+      $scope.setMessage = function() {
+        childRef.set({
+          user: 'Mércio',
+          text: 'Sucesso'
+        });
+        console.log('função executada');
+        
+      };
   });
