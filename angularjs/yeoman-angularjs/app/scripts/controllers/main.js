5d3d9d929d0a78b1cc9e57cf7b6dcc7666635a03
merciof <mercio.andrade@ufrpe.br>
2018-04-16 23:33:48 -0300

    adição de interação em tempo real com o firebase

--- angularjs/yeoman-angularjs/app/scripts/controllers/main.js
+++ angularjs/yeoman-angularjs/app/scripts/controllers/main.js
@@ -8,37 +8,56 @@
  * Controller of the firebaseAppApp
  */
 angular.module('firebaseAppApp')
-  .controller('MainCtrl', function ($scope) {
-      //**Forma antiga de instanciar o objeto firebase
-      // var rootRef = new Firebase('https://projeto-teste-7dcf3.firebaseio.com/');
-      // var childRef = rootRef.child('message'); */
-      
-      var config = {
-        apiKey: "AIzaSyD_RJeXxSxpw7LXZ5RWK_zUWwGXR7nv3M4",
-        authDomain: "projeto-teste-7dcf3.firebaseapp.com",
-        databaseURL: "https://projeto-teste-7dcf3.firebaseio.com"
-      };
-      
-      firebase.initializeApp(config);
+  .controller('MainCtrl', function ($scope, $timeout) {
+    //**Forma antiga de instanciar o objeto firebase
+    // var rootRef = new Firebase('https://projeto-teste-7dcf3.firebaseio.com/');
+    // var childRef = rootRef.child('message'); */
 
-      var rootRef = firebase.database().ref();
-      var childRef = rootRef.child('message');
-      
-      $scope.setMessage = function() {
-        childRef.set({
-          user: 'Mércio',
-          text: 'Sucesso'
-        });
-        console.log('função executada');
-        
-      };
+    var config = {
+      apiKey: "AIzaSyD_RJeXxSxpw7LXZ5RWK_zUWwGXR7nv3M4",
+      authDomain: "projeto-teste-7dcf3.firebaseapp.com",
+      databaseURL: "https://projeto-teste-7dcf3.firebaseio.com"
+    };
 
-      $scope.updateMessage = function(){
-        childRef.update({text: 'Bye'});
-        console.log('função executada');
+    firebase.initializeApp(config);
+
+    var rootRef = firebase.database().ref();
+    var childRef = rootRef.child('message');
+
+    /** on() function: Listens for data changes at a particular location.
+        This is the primary way to read data from a Database. 
+        Your callback will be triggered for the initial data and again whenever the data changes.*/
+    childRef.on('value'/*'value' is a type of event*/, function (snapshot) {
+      $timeout(function () {
+        var snapshotVal = snapshot.val();
+        console.log(snapshotVal);
+        $scope.message = snapshotVal;
+      });
+    });
+
+    $scope.$watch('message.text', function (newVal) {
+      if(!newVal) {
+        return;
       }
+      childRef.update({text: newVal});
 
-      $scope.deleteMessage = function() {
-        childRef.remove();
-      }
+    });
+
+    $scope.setMessage = function () {
+      childRef.set({
+        user: 'Mércio',
+        text: 'Sucesso'
+      });
+      console.log('função executada');
+
+    };
+
+    $scope.updateMessage = function () {
+      childRef.update({ text: 'Bye' });
+      console.log('função executada');
+    }
+
+    $scope.deleteMessage = function () {
+      childRef.remove();
+    }
   });
