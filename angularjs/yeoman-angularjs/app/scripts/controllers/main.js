d6bfba7c360a2795f2781f5deada820a47a50253
merciof <mercio.andrade@ufrpe.br>
2018-04-18 22:18:46 -0300

    inserção de objeto no firebase após clique em botão

--- angularjs/yeoman-angularjs/app/scripts/controllers/main.js
+++ angularjs/yeoman-angularjs/app/scripts/controllers/main.js
@@ -22,42 +22,57 @@ angular.module('firebaseAppApp')
     firebase.initializeApp(config);
 
     var rootRef = firebase.database().ref();
-    var childRef = rootRef.child('message');
+    var messagesRef = rootRef.child('messages');
+
+    $scope.currentUser = null;
+    $scope.currentText = null;
 
     /** on() function: Listens for data changes at a particular location.
         This is the primary way to read data from a Database. 
         Your callback will be triggered for the initial data and again whenever the data changes.*/
-    childRef.on('value'/*'value' is a type of event*/, function (snapshot) {
+    messagesRef.on('value'/*'value' is a type of event*/, function (snapshot) {
       $timeout(function () {
         var snapshotVal = snapshot.val();
         console.log(snapshotVal);
-        $scope.message = snapshotVal;
+        $scope.messages = snapshotVal;
       });
     });
 
-    $scope.$watch('message.text', function (newVal) {
-      if(!newVal) {
-        return;
-      }
-      childRef.update({text: newVal});
+    $scope.sendMessage = function () {
+      alert('função executada');
+      var newMessage = {
+        user: $scope.currentUser,
+        text: $scope.currentText
+      };
 
-    });
+      messagesRef.push(newMessage);
 
-    $scope.setMessage = function () {
-      childRef.set({
-        user: 'Mércio',
-        text: 'Sucesso'
-      });
-      console.log('função executada');
 
-    };
-
-    $scope.updateMessage = function () {
-      childRef.update({ text: 'Bye' });
-      console.log('função executada');
     }
 
-    $scope.deleteMessage = function () {
-      childRef.remove();
-    }
+    // $scope.$watch('message.text', function (newVal) {
+    //   if(!newVal) {
+    //     return;
+    //   }
+    //   childRef.update({text: newVal});
+
+    // });
+
+    // $scope.setMessage = function () {
+    //   childRef.set({
+    //     user: 'Mércio',
+    //     text: 'Sucesso'
+    //   });
+    //   console.log('função executada');
+
+    // };
+
+    // $scope.updateMessage = function () {
+    //   childRef.update({ text: 'Bye' });
+    //   console.log('função executada');
+    // }
+
+    // $scope.deleteMessage = function () {
+    //   childRef.remove();
+    // }
   });
