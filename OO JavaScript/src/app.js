5ef46eeead92f5e7f2ad04c2485f3433da96c087
merciof <mercio.andrade@ufrpe.br>
2018-06-09 22:25:03 -0300

    criação das classes básicas

--- OO JavaScript/src/app.js
+++ OO JavaScript/src/app.js
@@ -1,1 +1,10 @@
+import { Car } from "./classes/car.js";
+import { Drone } from "./classes/drone.js";
+
+let c =  new Car();
+let d = new Drone();
+
+
 window.console.log('In app.js');
+window.console.log(c);
+window.console.log(d);
