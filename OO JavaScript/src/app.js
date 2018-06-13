b3fca1c71f96efda75e5d4f6126e046682be56c4
merciof <mercio.andrade@ufrpe.br>
2018-06-13 00:25:15 -0300

    adição da classe Image que possibilita  a inclusão dinâmica de imagens ao DOM. Adição de plugin do Babel no SystemJS para a transpilação de modulos. E correção de erro no Import do JQuery.

--- OO JavaScript/src/app.js
+++ OO JavaScript/src/app.js
@@ -1,11 +1,10 @@
-import { $ } from "jquery";
+import  $  from "jquery";
 import { Car } from "./classes/car.js";
 import { Drone } from "./classes/drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleet-data-service.js";
 import { Button } from "./ui/button.js";
-
-
+import { Image } from "./ui/image.js";
 
 
 // let dataService = new FleetDataService();
@@ -23,9 +22,12 @@ import { Button } from "./ui/button.js";
 //     window.console.log(drone.license);
 // }
 
-for (const error of dataService.errors) {
-    window.console.log(error.message);
-}
+// for (const error of dataService.errors) {
+//     window.console.log(error.message);
+// }
 
 let b = new Button('Clique aqui');
 b.appendToElement($('body'));
+
+let i = new Image('images/drone.jpg');
+i.appendToElement($('body'));
--- OO JavaScript/src/ui/button.js
+++ OO JavaScript/src/ui/button.js
@@ -1,4 +1,4 @@
-import { BaseElement } from "./base-element";
+import { BaseElement } from "./base-element.js";
 
 export class Button extends BaseElement{
     constructor(title){
