9bc2840e4d975bb8b6fda19a1ea3e88ef84073da
merciof <mercio.andrade@ufrpe.br>
2018-06-27 23:59:34 -0300

    criação da classe base e da classe app

--- OO JavaScript/src/app.js
+++ OO JavaScript/src/app.js
@@ -1,47 +1,17 @@
 import $ from "jquery";
-import { Car } from "./classes/car.js";
-import { Drone } from "./classes/drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleet-data-service.js";
-import { Button } from "./ui/button.js";
-import { Image } from "./ui/image.js";
-import { TitleBar } from "./ui/title-bar-mdl";
-import { DataTable } from "./ui/data-table-mdl";
+import { ApplicationBase } from "./framework/application-base";
 
+export class App extends ApplicationBase {
+    constructor(){
+        super('Fleet Manager');
+        this.dataService = new FleetDataService();
+        this.dataService.loadData(fleet);
+    }
+}
 
-
-// let dataService = new FleetDataService();
-// dataService.loadData(fleet);
-
-
-// window.console.log(dataService.cars);
-// window.console.log(dataService.drones);
-
-// for (const car of dataService.cars) {
-//     window.console.log(car.license);
-// }
-
-// for (const drone of dataService.drones) {
-//     window.console.log(drone.license);
-// }
-
-// for (const error of dataService.errors) {
-//     window.console.log(error.message);
-// }
-
-// let tb = new TitleBar('Our application');
-// tb.addLink('Home', '');
-// tb.addLink('Cars', '');
-// tb.addLink('Drones', '');
-// tb.addLink('Maps', '');
-// tb.appendToElement($('body'));
-// window.console.log(tb);
-
-let headers = 'License Make Model Miles'.split(' ');
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-let dt = new DataTable(headers, dataService.cars);
-dt.appendToElement($('body'));
+export let application = new App();
 
 
 
---
+++ OO JavaScript/src/framework/application-base.js
@@ -0,0 +1,9 @@
+import { TitleBar } from "../ui/title-bar-mdl";
+
+//contém código que será reutilizado na aplicação
+export class ApplicationBase {
+    constructor(title) {
+        this.title = title;
+        this.titleBar = new TitleBar(this.title);
+    }
+}
