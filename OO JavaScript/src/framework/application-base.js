d5a03331288892dc1c24bc3bf1f301d3bffa8043
merciof <mercio.andrade@ufrpe.br>
2018-06-29 13:00:30 -0300

    adição do método addRoute

--- OO JavaScript/src/framework/application-base.js
+++ OO JavaScript/src/framework/application-base.js
@@ -5,9 +5,18 @@ export class ApplicationBase {
     constructor(title) {
         this.title = title;
         this.titleBar = new TitleBar(this.title);
+        this.routeMap = {};
     }
 
-    show(element){
+    show(element) {
         this.titleBar.appendToElement(element);
+    }
+
+    addRoute(id, pageObject, defaultRoute = false) {
+        this.titleBar.addLink(id,'');
+        this.routeMap[id] = pageObject;
+        if(defaultRoute) {
+            this.defaultRoute = id;
+        }
     }
 }
