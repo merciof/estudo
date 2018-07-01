c88c6e21a04c8c2498ac08d1a318a6aa75cc15c2
merciof <mercio.andrade@ufrpe.br>
2018-07-01 00:07:50 -0300

    edição do HTML do Button retornado pelo método getElementString

--- OO JavaScript/src/ui/button.js
+++ OO JavaScript/src/ui/button.js
@@ -4,10 +4,16 @@ export class Button extends BaseElement{
     constructor(title){
         super();
         this.title = title;
+        this.styleString = '';
     }
     getElementString(){
-        return `<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        return `<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+                    style="${this.styleString}">
                     ${this.title}
                 </button>`
+    }
+
+    setStyleString(style) {
+        this.styleString = style;
     }
 }
