a55d1a5c0f970d46f57c59c2efdc902f03e9af7f
merciof <mercio.andrade@ufrpe.br>
2020-07-01 12:32:05 -0300

    Criação da classe Respeitoso.

---
+++ coursera-java-oo-formas-de-tratamento/src/Respeitoso.java
@@ -0,0 +1,23 @@
+
+//Respeitoso: deve receber em seu construtor a informação se é masculino ou feminino, e retornar "Sr." ou "Sra." seguido do sobrenome
+
+public class Respeitoso implements FormatadorNome {
+
+	String _sexo;
+	
+	Respeitoso(String sexo) {
+		_sexo = sexo;
+	}
+	
+	@Override
+	public String formatarNome(String nome, String sobrenome) {
+		if(_sexo == "masculino")
+			return "Sr." + nome;
+		if(_sexo == "feminino")
+			return "Sra." + nome;
+
+		throw new GeneroInvalidoException("Genero invalido");
+	
+	}
+
+}
