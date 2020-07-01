5e985cbc8dd7c0ec33c4f1817c8905a19b2a0f1e
merciof <mercio.andrade@ufrpe.br>
2020-07-01 17:54:28 -0300

    Criacão da classe ComTítulo

---
+++ coursera-java-oo-formas-de-tratamento/src/ComTitulo.java
@@ -0,0 +1,22 @@
+
+
+//ComTítulo: deve receber em seu construtor o título e retornar o título seguido de nome e sobrenome. Exemplo: "Magnífico Pedro Cabral"
+
+public class ComTitulo implements FormatadorNome {
+
+	String _titulo;
+	
+	
+	public ComTitulo(String _titulo) {
+		super();
+		this._titulo = _titulo;
+	}
+
+
+	@Override
+	public String formatarNome(String nome, String sobrenome) {
+		
+		return _titulo + nome + sobrenome;
+	}
+
+}
