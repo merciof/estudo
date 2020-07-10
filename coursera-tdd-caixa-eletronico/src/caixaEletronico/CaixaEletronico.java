6e88ea8b0a9b81e4573f4d05864233741db93c1a
merciof <mercio.andrade@ufrpe.br>
2020-07-10 19:36:30 -0300

    adição dos métodos

--- coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
+++ coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
@@ -2,4 +2,20 @@ package caixaEletronico;
 
 public class CaixaEletronico {
 
+	public String logar() {
+		return "retorno logar";
+	}
+
+	public String sacar() {
+		return "retorno sacar";
+	}
+
+	public String depositar() {
+		return "retorno depositar";
+	}
+
+	public Object saldo() {
+		return "retorno saldo";
+	}
+
 }
