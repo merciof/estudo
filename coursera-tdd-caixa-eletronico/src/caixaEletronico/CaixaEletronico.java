0fa396c3dd995c3b43909be859b1a2050148c59c
merciof <mercio.andrade@ufrpe.br>
2020-07-11 17:55:14 -0300

    implementação do método logar()

--- coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
+++ coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
@@ -2,8 +2,13 @@ package caixaEletronico;
 
 public class CaixaEletronico {
 
-	public String logar() {
-		return "retorno logar";
+	
+	public String logar(ContaCorrente contaCorrente, String login, String senha) {
+		
+		if(contaCorrente.login == login && contaCorrente.senha == senha)
+			return "Usuário Autenticado";
+		
+		return "login ou senha invalidos";
 	}
 
 	public String sacar() {
