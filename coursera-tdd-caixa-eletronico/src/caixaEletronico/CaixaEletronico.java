fc10eb01e69789fa308cf6ae5532de707625f607
merciof <mercio.andrade@ufrpe.br>
2020-07-11 19:48:52 -0300

    adição método logar usando Mock

--- coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
+++ coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
@@ -3,14 +3,24 @@ package caixaEletronico;
 //Ao executar o método sacar(), e a execução for com sucesso, deve retornar a mensagem "Retire seu dinheiro". Se o valor sacado for maior que o saldo da conta, a classe CaixaEletronico deve retornar uma String dizendo "Saldo insuficiente".
 
 public class CaixaEletronico {
-
 	
+	MockServicoRemoto _mockServicoRemoto = MockServicoRemoto.getInstance();
+	
+//	public CaixaEletronico() {
+//		super();
+//		_mockServicoRemoto = new MockServicoRemoto();
+//	}
+
 	public String logar(ContaCorrente contaCorrente, String login, String senha) {
 		
-		if(contaCorrente.login == login && contaCorrente.senha == senha)
-			return "Usuário Autenticado";
+		if (_mockServicoRemoto.recuperarConta(contaCorrente.numero).login == login
+					&& _mockServicoRemoto.recuperarConta(contaCorrente.numero).senha  == senha) {
+				return "Usuário Autenticado";
+			}
 		
-		return "login ou senha invalidos";
+			return "Não foi possível autenticar o usuário";
+		
+		//return "login ou senha invalidos";
 	}
 
 	public String sacar(ContaCorrente contaCorrente, float valor) {
@@ -20,12 +30,13 @@ public class CaixaEletronico {
 		return "Retire seu dinheiro";
 	}
 
-	public String depositar() {
-		return "retorno depositar";
+	public String depositar(ContaCorrente contaCorrente, float valor) {
+		contaCorrente.setSaldo(contaCorrente.getSaldo() + valor);
+		return "Depósito recebido com sucesso";
 	}
 
-	public Object saldo() {
-		return "retorno saldo";
+	public Object saldo(ContaCorrente contaCorrente) {
+		return "O saldo é R$" + contaCorrente.getSaldo();
 	}
 
 }
