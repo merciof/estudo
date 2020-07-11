78575329233b4a82929a07df2a031a54432f0a98
merciof <mercio.andrade@ufrpe.br>
2020-07-11 18:02:20 -0300

    implementação do método sacar

--- coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
+++ coursera-tdd-caixa-eletronico/src/caixaEletronico/CaixaEletronico.java
@@ -1,4 +1,6 @@
 package caixaEletronico;
+//
+//Ao executar o método sacar(), e a execução for com sucesso, deve retornar a mensagem "Retire seu dinheiro". Se o valor sacado for maior que o saldo da conta, a classe CaixaEletronico deve retornar uma String dizendo "Saldo insuficiente".
 
 public class CaixaEletronico {
 
@@ -11,8 +13,11 @@ public class CaixaEletronico {
 		return "login ou senha invalidos";
 	}
 
-	public String sacar() {
-		return "retorno sacar";
+	public String sacar(ContaCorrente contaCorrente, float valor) {
+		if(contaCorrente.getSaldo() < valor)
+			return "Saldo insuficiente";
+		
+		return "Retire seu dinheiro";
 	}
 
 	public String depositar() {
