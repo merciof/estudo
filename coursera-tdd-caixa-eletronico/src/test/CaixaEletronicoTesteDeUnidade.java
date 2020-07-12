4ce6e19379638ccbe1bc8ee759a8ceeba7163c6d
merciof <mercio.andrade@ufrpe.br>
2020-07-12 20:38:08 -0300

    adição de testes de unidade

--- coursera-tdd-caixa-eletronico/src/test/CaixaEletronicoTesteDeUnidade.java
+++ coursera-tdd-caixa-eletronico/src/test/CaixaEletronicoTesteDeUnidade.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import caixaEletronico.CaixaEletronico;
 import caixaEletronico.ContaCorrente;
+import caixaEletronico.FalhaNoHardareException;
+import caixaEletronico.MockHardware;
 import caixaEletronico.MockServicoRemoto;
 
 public class CaixaEletronicoTesteDeUnidade {
@@ -15,12 +17,17 @@ public class CaixaEletronicoTesteDeUnidade {
 	CaixaEletronico _caixaEletronico;
 	ContaCorrente _contaCorrente;
 	MockServicoRemoto _mockServicoRemoto;
+	MockHardware _mockHardware;
+	MockHardware _mockHardwareComDefeito;
+	
 	
 	@Before
 	public void criacaoDeObjetos() {
 		_caixaEletronico = new CaixaEletronico();
 		_contaCorrente = new ContaCorrente();
 		_mockServicoRemoto = MockServicoRemoto.getInstance();
+		_mockHardware = new MockHardware(false);
+		_mockHardwareComDefeito = new MockHardware(true);;
 		_mockServicoRemoto._listaDeContas.put(1, _contaCorrente);
 		_mockServicoRemoto.recuperarConta(1).numero = 1;
 		_mockServicoRemoto.recuperarConta(1).login = "login";
@@ -29,40 +36,67 @@ public class CaixaEletronicoTesteDeUnidade {
 	}
 	
 	@Test
-	public void testLogarComSucesso() {
-		
+	public void testLogarComSucesso() throws FalhaNoHardareException {
+		_mockHardware.pegarNumeroDaContaCartao();
 		assertEquals("Usuário Autenticado", _caixaEletronico.logar(_contaCorrente, "login", "senha"));
 	}
 	
+	@Test(expected = FalhaNoHardareException.class)
+	public void testLogarComFalhaNoHardware() throws FalhaNoHardareException {
+		_mockHardwareComDefeito.pegarNumeroDaContaCartao();
+	}
+	
 	@Test
-	public void testLogarFalha() {
+	public void testLogarDadosDeLoginIncorretos() {
 		
 		assertEquals("Não foi possível autenticar o usuário", _caixaEletronico.logar(_contaCorrente, "usu", "123"));
 	}
 	
 	@Test
-	public void testSacar() {
+	public void testSacar() throws FalhaNoHardareException {
+		_mockHardware.pegarNumeroDaContaCartao();
 		_caixaEletronico.logar(_contaCorrente, "login", "senha");
 		_mockServicoRemoto.recuperarConta(_contaCorrente.numero).setSaldo(500);
+		_mockHardware.entregarDinheiro();
 		assertEquals("Retire seu dinheiro", _caixaEletronico.sacar(_contaCorrente, 100));
 	}
 	
+	@Test(expected = FalhaNoHardareException.class)
+	public void testSacarComFalhaNoHardware() throws FalhaNoHardareException {
+		_caixaEletronico.logar(_contaCorrente, "login", "senha");
+		_mockServicoRemoto.recuperarConta(_contaCorrente.numero).setSaldo(500);
+		_mockHardwareComDefeito.entregarDinheiro();
+	}
+	
 	@Test
-	public void testSacarSaldoInsuficiente() {
+	public void testSacarSaldoInsuficiente() throws FalhaNoHardareException {
+		_mockHardware.pegarNumeroDaContaCartao();
 		_caixaEletronico.logar(_contaCorrente, "login", "senha");
 		_mockServicoRemoto.recuperarConta(_contaCorrente.numero).setSaldo(50);
 		assertEquals("Saldo insuficiente", _caixaEletronico.sacar(_contaCorrente, 100));
 	}
 	
 	@Test
-	public void testDepositar() {
+	public void testDepositar() throws FalhaNoHardareException {
+		_mockHardware.pegarNumeroDaContaCartao();
 		_caixaEletronico.logar(_contaCorrente, "login", "senha");
+		_mockHardware.lerEnvelope();
 		assertEquals("Retire seu dinheiro", _caixaEletronico.depositar(_contaCorrente, 50));
 		assertEquals(50, _mockServicoRemoto.recuperarConta(_contaCorrente.numero).getSaldo(), 1);
 	}
 	
+	@Test(expected = FalhaNoHardareException.class)
+	public void testDepositarComFalhaNoHardware() throws FalhaNoHardareException {
+		_caixaEletronico.logar(_contaCorrente, "login", "senha");
+		_mockHardwareComDefeito.lerEnvelope();
+		assertEquals("Retire seu dinheiro", _caixaEletronico.depositar(_contaCorrente, 50));
+		assertEquals(50, _mockServicoRemoto.recuperarConta(_contaCorrente.numero).getSaldo(), 1);
+	}
+	
+	
 	@Test
-	public void testSaldo() {
+	public void testSaldo() throws FalhaNoHardareException {
+		_mockHardware.pegarNumeroDaContaCartao();
 		_caixaEletronico.logar(_contaCorrente, "login", "senha");
 		assertEquals("O saldo é R$0.0", _caixaEletronico.saldo(_contaCorrente));
 	}
