51d7f153fd23076c54835f3f4f07940abb02c2bc
merciof <mercio.andrade@ufrpe.br>
2020-07-15 19:18:01 -0300

    criação da interface IEmbaralhador

---
+++ coursera-java-oo-projeto-final/src/pacote/IEmbaralhador.java
@@ -0,0 +1,9 @@
+package pacote;
+
+//interface que representa classes reponsáveis por receber uma palavra e
+//retornar ela embaralhada. Pelo menos duas implementações deverão ser feitas.
+public interface IEmbaralhador {
+
+	public String embaralhar(String palavra);
+	
+}
