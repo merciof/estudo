4a56223e16afa18a992b5a7c3fa08e55e4bbafc8
merciof <mercio.andrade@ufrpe.br>
2020-07-15 19:17:36 -0300

    criação da classe BancoDePalavras

---
+++ coursera-java-oo-projeto-final/src/pacote/BancoDePalavras.java
@@ -0,0 +1,35 @@
+package pacote;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Random;
+
+//classe que possui um método que retorna uma palavra retirada
+//aleatóriamente de uma lista de palavras lida a partir de um arquivo.
+public class BancoDePalavras {
+	
+
+	private static List<String> lines = null;
+	
+	private Random rand;
+	
+
+	public BancoDePalavras() {
+		super();
+		rand = new Random();
+	}
+
+	static {
+	    try {
+	        lines = Files.readAllLines(new File("banco-de-palavras.txt").toPath());
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	protected String getPalavra() {
+		return lines.get(rand.nextInt(lines.size()));
+	}
+	
+}
