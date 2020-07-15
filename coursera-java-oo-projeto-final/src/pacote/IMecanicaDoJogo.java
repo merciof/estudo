0cac1072ddc5aee2f3b07441b01b148856edcd51
merciof <mercio.andrade@ufrpe.br>
2020-07-15 19:18:34 -0300

    criação da interface IMecanicaDoJogo

---
+++ coursera-java-oo-projeto-final/src/pacote/IMecanicaDoJogo.java
@@ -0,0 +1,8 @@
+package pacote;
+//interface que representa o andamento e a lógica do jogo. É responsável
+//por ditar o andamento do jogo. Ela que vai dizer se o jogo acabou ou não, se o usuário acertou a
+//palavra ou não, se o usuário pode tentar acertar a palavra novamente e qual foi a pontuação final
+//do jogador. Pelo menos duas implementações dessa interface devem ser criadas.
+public interface IMecanicaDoJogo {
+
+}
