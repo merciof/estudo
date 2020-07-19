391236514186a1f03a8c2cb7037cdf130462705c
merciof <mercio.andrade@ufrpe.br>
2020-07-19 20:12:05 -0300

    criação da classe FabricaEmbaralhador

---
+++ coursera-java-oo-projeto-final/src/pacote/FabricaEmbaralhador.java
@@ -0,0 +1,29 @@
+package pacote;
+
+import java.util.Random;
+
+//possui um método que retorna um embaralhador
+//aleatóriamente.
+public class FabricaEmbaralhador {
+	
+	static Random randon = new Random();
+	static int limite = 20;
+	static int valorUm = 0;
+	static int valorDois = 0;
+	
+	static void gerarAleatoriedade() {
+		valorUm = randon.nextInt(limite);
+		valorDois = randon.nextInt(limite);
+	}
+	
+	
+	static IEmbaralhador getInstance() {
+		
+		gerarAleatoriedade();
+		
+		if(valorUm > valorDois)
+			return new EmbaralhadorUm();
+		else
+			return new EmbaralhadorUm();
+	}
+}
