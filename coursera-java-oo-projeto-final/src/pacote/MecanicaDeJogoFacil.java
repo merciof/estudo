6631ba85ac11df6f25312086956f68d714f13934
merciof <mercio.andrade@ufrpe.br>
2020-07-19 20:13:27 -0300

    Criação da classe MecânicaDeJogoFacil

---
+++ coursera-java-oo-projeto-final/src/pacote/MecanicaDeJogoFacil.java
@@ -0,0 +1,55 @@
+package pacote;
+
+public class MecanicaDeJogoFacil implements IMecanicaDoJogo {
+
+	int _numeroMaximoDeTentativas;
+	
+	int _pontuacaoPorAcerto;
+	
+	int _pontuacaoDoJogador;
+	
+
+	public MecanicaDeJogoFacil() {
+		super();
+		_numeroMaximoDeTentativas = 2;
+		_pontuacaoPorAcerto = 1;
+		_pontuacaoDoJogador = 0;
+	}
+	
+
+	public int get_pontuacaoDoJogador() {
+		return _pontuacaoDoJogador;
+	}
+
+
+	@Override
+	public int somarPonto() {
+		// TODO Auto-generated method stub
+		return _pontuacaoDoJogador += _pontuacaoPorAcerto;
+	}
+
+
+
+	@Override
+	public int removerTentativa() {
+		// TODO Auto-generated method stub
+		return _numeroMaximoDeTentativas -= 1;
+	}
+
+
+	@Override
+	public boolean fimDoJogo() {
+		if ( _numeroMaximoDeTentativas > 0) 
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public boolean veriricarTentativa(String stringDoBancoDePalavras, String stringDigitadaPeloJogador) {
+		
+		return stringDoBancoDePalavras.equalsIgnoreCase(stringDigitadaPeloJogador);
+	}
+
+	
+}
