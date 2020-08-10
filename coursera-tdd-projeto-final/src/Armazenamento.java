d2095dbfad294d45fb7aa233acf74dc9132ae4aa
merciof <mercio.andrade@ufrpe.br>
2020-08-10 10:05:02 -0300

    criação de esqueleto de Armazenamento

---
+++ coursera-tdd-projeto-final/src/Armazenamento.java
@@ -0,0 +1,35 @@
+
+/**
+ * 
+ * @author merciof
+ * 
+ * 
+ *
+ */
+
+public class Armazenamento {
+	
+	//Armazenar que um usuário recebeu uma quantidade de um tipo de ponto. 
+	//Por exemplo: o usuário "guerra" recebeu "10" pontos do tipo "estrela"
+	void armazenarPontos(String nomeUsuario, String tipoDePonto, int quantidadeDePontos) {
+		
+	}
+	
+	//Recuperar quantos pontos de um tipo tem um usuário. 
+	//Por exemplo: retornar quantos pontos do tipo "estrela" tem o usuário "guerra"
+	int getPontosDeUsuarioPorTipo(String nomeUsuario, String tipoDePonto) {
+		return 0;
+	}
+	
+	//Retornar todos os usuários que já receberam algum tipo de ponto.
+	String getUsuariosComPontuacao() {
+		return " ";
+	}
+	
+	//Retornar todos os tipos de ponto que já foram registrados para algum usuário.
+	String getTiposDePontosRegistrados() {
+		return null;
+		
+	}
+	
+}
